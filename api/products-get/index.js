18dc55bd521e4ee106c57e83cd45c253c5cb8bbd
John Papa <johnpapa@users.noreply.github.com>
2022-10-04 15:57:32 +0000

    one more change, yet again

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -3,7 +3,19 @@ const data = require('../shared/product-data');
 module.exports = async function (context, req) {
   try {
     const products = data.getProducts();
-    context.res.status(200).json(products);
+    // binary tree sort algorithm to sort products by name in ascending order 
+
+    const sortedProducts = products.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+
+    context.res.status(200).json(sortedProducts);
   } catch (error) {
     context.res.status(500).send(error);
   }
