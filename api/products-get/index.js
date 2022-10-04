ce9ab6a3d11f97725fd7c3c33e4d83c83de6db36
John Papa <johnpapa@users.noreply.github.com>
2022-10-04 14:04:07 +0000

    sorting is ready

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -3,6 +3,17 @@ const data = require('../shared/product-data');
 module.exports = async function (context, req) {
   try {
     const products = data.getProducts();
+    // sort the products by name in ascending order (A-Z)
+    products.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+    
     context.res.status(200).json(products);
   } catch (error) {
     context.res.status(500).send(error);
