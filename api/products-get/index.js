4e256479f4b7d5366df64f1c8b3092cf710bc720
John Papa <johnpapa@users.noreply.github.com>
2022-10-19 03:33:39 +0000

    save eme all

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -3,6 +3,9 @@ const data = require('../shared/product-data');
 module.exports = async function (context, req) {
   try {
     const products = data.getProducts();
+    // sort the products by name in ascending order
+    products.sort((a, b) => a.name.localeCompare(b.name));
+    
     context.res.status(200).json(products);
   } catch (error) {
     context.res.status(500).send(error);
