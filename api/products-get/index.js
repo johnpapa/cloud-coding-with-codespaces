1d591aa242daff79646fe419cac0385fdf276de2
John Papa <johnpapa@users.noreply.github.com>
2022-11-02 18:52:34 +0000

    yep, i love this sort

--- api/products-get/index.js
+++ api/products-get/index.js
@@ -3,6 +3,17 @@ const data = require('../shared/product-data');
 module.exports = async function (context, req) {
   try {
     const products = data.getProducts();
+    // sort products by name in ascending order
+    products.sort((a, b) => {
+      if (a.name < b.name) {
+        return -1;
+      } else if (a.name > b.name) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
     context.res.status(200).json(products);
   } catch (error) {
     context.res.status(500).send(error);
