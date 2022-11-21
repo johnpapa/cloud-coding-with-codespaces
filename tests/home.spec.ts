acd6410038f9ece09ebd570f8b9a6954b5311056
John Papa <john@johnpapa.net>
2022-11-20 21:38:47 -0500

    prettier on the spec

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -5,13 +5,13 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Homepage has List with specified title', async ({ page }) => {
-  const listTitle=page.locator('.content-title-group h2');
-  await expect(listTitle).toHaveText("My List");
+  const listTitle = page.locator('.content-title-group h2');
+  await expect(listTitle).toHaveText('My List');
 });
 
 test('Homepage has List with at least 3 items', async ({ page }) => {
-  // Uses poll to get greaterThan 
+  // Uses poll to get greaterThan
   // See: https://github.com/microsoft/playwright/issues/10367
-  const list=page.locator('.card');
-  await expect.poll( () => list.count() ).toBeGreaterThanOrEqual(3);
+  const list = page.locator('.card');
+  await expect.poll(() => list.count()).toBeGreaterThanOrEqual(3);
 });
